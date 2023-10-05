519ee4d34a3bac0727a48b8573bdf89815bce39d
Mohammadreza Mirdar <mohammadrezamirdar2001@gmail.com>
2023-10-05 11:16:44 +0330

    Common application developed

---
+++ Common/Common.Application/Validation/FluentValidation/FluentValidation.cs
@@ -0,0 +1,57 @@
+﻿using Common.Application.FileUtil;
+using Common.Application.SecurityUtil;
+using Common.Domain;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace Common.Application.Validation.FluentValidation
+{
+    public static class FluentValidation
+    {
+        public static IRuleBuilderOptionsConditions<T, TProperty> JustImageFile<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder, string errorMessage = "Upload pictures is just allowed") where TProperty : IFormFile?
+        {
+            return ruleBuilder.Custom((file, context) =>
+            {
+                if (file == null)
+                    return;
+
+                if (!ImageValidator.IsImage(file))
+                {
+                    context.AddFailure(errorMessage);
+                }
+            });
+        }
+
+        public static IRuleBuilderOptionsConditions<T, string> ValidNationalId<T>(this IRuleBuilder<T, string> ruleBuilder, string errorMessage = "National code is not valid")
+        {
+            return ruleBuilder.Custom((nationalCode, context) =>
+            {
+                if (IranianNationalIdChecker.IsValid(nationalCode) == false)
+                    context.AddFailure(errorMessage);
+            });
+        }
+        public static IRuleBuilderOptionsConditions<T, string> ValidPhoneNumber<T>(this IRuleBuilder<T, string> ruleBuilder, string errorMessage = "Phone number is not valid")
+        {
+            return ruleBuilder.Custom((phoneNumber, context) =>
+            {
+                if (string.IsNullOrWhiteSpace(phoneNumber) || phoneNumber.Length is < 11 or > 11)
+                    context.AddFailure(errorMessage);
+
+            });
+        }
+
+        public static IRuleBuilderOptionsConditions<T, TProperty> JustValidFile<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder, string errorMessage = "File is not valid") where TProperty : IFormFile
+        {
+            return ruleBuilder.Custom((file, context) =>
+            {
+                if (file == null)
+                    return;
+
+                if (!FileValidation.IsValidFile(file))
+                {
+                    context.AddFailure(errorMessage);
+                }
+            });
+        }
+    }
+}
---
+++ Common/Common.Application/Validation/ValidationMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Common.Application.Validation
+{
+    public static class ValidationMessages
+    {
+        public const string Required = "This field is required";
+        public const string InvalidPhoneNumber = "Phone number is not valid";
+        public const string NotFound = "Not found";
+        public const string MaxLength = "The number of characters entered exceeds the limit";
+        public const string MinLength = "The number of characters entered is less than the limit";
+
+        public static string required(string field) => $"{field} is required ";
+        public static string maxLength(string field, int maxLength) => $"{field} must be less than {maxLength} character(s)";
+        public static string minLength(string field, int minLength) => $"{field} must be more than {minLength} character(s)";
+    }
+}
