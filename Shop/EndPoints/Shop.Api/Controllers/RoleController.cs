e7717443188ed036d7b9bb1c0ab2415d3a451d32
Mohammadreza Mirdar <mohammadrezamirdar2001@gmail.com>
2023-10-22 20:57:46 +0330

    Develop role controller

---
+++ Shop/EndPoints/Shop.Api/Controllers/RoleController.cs
@@ -0,0 +1,43 @@
+﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Roles.Create;
+using Shop.Application.Roles.Edit;
+using Shop.Presentation.facade.Roles;
+using Shop.Query.Roles.DTOs;
+
+namespace Shop.Api.Controllers
+{
+    public class RoleController : ApiController
+    {
+        private readonly IRoleFacade _roleFacade;
+
+        public RoleController(IRoleFacade roleFacade)
+        {
+            _roleFacade = roleFacade;
+        }
+
+        [HttpGet("{roleId}")]
+        public async Task<ApiResult<RoleDto?>> GetRoleById(long roleId)
+        {
+            return QueryResult(await _roleFacade.GetById(roleId));
+        }
+
+        [HttpGet]
+        public async Task<ApiResult<List<RoleDto>>> GetRoleList()
+        {
+            return QueryResult(await _roleFacade.GetList());
+        }
+
+        [HttpPost]
+        public async Task<ApiResult> CreateRole(CreateRoleCommand command)
+        {
+            return CommandResult(await _roleFacade.Create(command));
+        }
+
+        [HttpPut]
+        public async Task<ApiResult> EditRole(EditRoleCommand command)
+        {
+            return CommandResult(await _roleFacade.Edit(command));
+        }
+    }
+}
